83ab4411e68fb62d777c10e5e101d6f92926d525
chi057 <zlin90228@gmail.com>
2023-06-05 15:56:51 +0800

    2023/0605 - 程式撰寫
    1.完成下拉式選單程式碼
    2.完成顯示時間、日期與星期幾的程式碼
    3.加入鬧鐘功能的程式碼
    4.完成設定&取消鬧鐘的Click事件綁定，再加入程式碼
    5.加入預先停止鬧鐘鈴聲的程式碼和讓鬧鐘鈴聲可重複播放的程式碼
    6.更改按鍵&文字框位置
    7.更改文字框背景
    8.更改時間文字框的背景顏色
    9.更改下拉式選單的字型大小

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Windows.Threading;
 
 namespace _8thMeet
 {
@@ -20,9 +21,62 @@ namespace _8thMeet
     /// </summary>
     public partial class MainWindow : Window
     {
+        List<string> hours = new List<string>();
+        List<string> minutes = new List<string>();
+        DispatcherTimer timer = new DispatcherTimer();
+        string strSelectTime = "";
+        DispatcherTimer timerAlert = new DispatcherTimer();
         public MainWindow()
         {
             InitializeComponent();
+
+            for (int i = 0; i <= 23; i++)
+                hours.Add(string.Format("{0:00}", i));
+            for (int i = 0; i <= 59; i++)
+                minutes.Add(string.Format("{0:00}", i));
+            CmbHour.ItemsSource = hours;
+            CmbMin.ItemsSource = minutes;
+
+            timer.Interval = TimeSpan.FromSeconds(1);
+            timer.Tick += new EventHandler(Timer_tick);
+            timer.Start();
+            timerAlert.Interval = TimeSpan.FromSeconds(1);
+            timerAlert.Tick += new EventHandler(TimerAlert_tick);
+            MeSound.LoadedBehavior = MediaState.Stop;
+        }
+        private void Timer_tick(object sender, EventArgs e)
+        {
+            TxtTime.Text = DateTime.Now.ToString("HH:mm:ss");  
+            TxtDate.Text = DateTime.Now.ToString("yyyy-MM-dd");
+            TxtWeekDay.Text = DateTime.Now.ToString("dddd");
+        }
+        private void TimerAlert_tick(object sender, EventArgs e)
+        {
+            if (strSelectTime == DateTime.Now.ToString("HH:mm"))
+            {
+                MeSound.LoadedBehavior = MediaState.Play;
+                timerAlert.Stop();
+            }
+        }
+        private void BtnSetAlert_Click(object sender, RoutedEventArgs e)
+        {
+            timerAlert.Start();
+            BtnSetAlert.IsEnabled = false;
+            BtnCancelAlert.IsEnabled = true;
+            strSelectTime = CmbHour.SelectedItem + ":" + CmbMin.SelectedItem;
+        }
+
+        private void BtnCancelAlert_Click(object sender, RoutedEventArgs e)
+        {
+            MeSound.LoadedBehavior = MediaState.Stop;
+            timerAlert.Stop();
+            BtnSetAlert.IsEnabled = true;
+            BtnCancelAlert.IsEnabled = false;
+        }
+        private void meSound_MediaEnded(object sender, RoutedEventArgs e)
+        {
+            MeSound.Position = new TimeSpan(0, 0, 1);
+            MeSound.LoadedBehavior = MediaState.Play;
         }
     }
 }
