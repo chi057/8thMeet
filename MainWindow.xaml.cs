aed1fbd19a95096b91efd0c211984c191c29d445
chi057 <zlin90228@gmail.com>
2023-06-06 10:03:55 +0800

    2023/0606 - 加入頁籤和介面設計
    1.加入頁籤(時鐘、碼表、倒數)
    2.設定(時鐘、碼表、倒數)頁籤背景並加上圖源
    3.碼表基本介面設計
    4.碼表按鍵事件綁定&撰寫程式碼
    5.介面調整及美化(時鐘、碼表)
    6.微調碼表紀錄顯示框的程式碼

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -13,6 +13,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 using System.Windows.Threading;
+using System.Diagnostics;
 
 namespace _8thMeet
 {
@@ -21,15 +22,21 @@ namespace _8thMeet
     /// </summary>
     public partial class MainWindow : Window
     {
+        // 時鐘
         List<string> hours = new List<string>();
         List<string> minutes = new List<string>();
         DispatcherTimer timer = new DispatcherTimer();
         string strSelectTime = "";
         DispatcherTimer timerAlert = new DispatcherTimer();
+        //碼表
+        List<string> StopWatchLog = new List<string>();
+        DispatcherTimer timerStopWatch = new DispatcherTimer();
+        Stopwatch sw = new Stopwatch();
+
         public MainWindow()
         {
             InitializeComponent();
-
+            //時鐘
             for (int i = 0; i <= 23; i++)
                 hours.Add(string.Format("{0:00}", i));
             for (int i = 0; i <= 59; i++)
@@ -43,7 +50,11 @@ namespace _8thMeet
             timerAlert.Interval = TimeSpan.FromSeconds(1);
             timerAlert.Tick += new EventHandler(TimerAlert_tick);
             MeSound.LoadedBehavior = MediaState.Stop;
+            //碼表
+            timerStopWatch.Interval = TimeSpan.FromMilliseconds(1);
+            timerStopWatch.Tick += new EventHandler(timerStopWatch_tick);
         }
+        //時鐘
         private void Timer_tick(object sender, EventArgs e)
         {
             TxtTime.Text = DateTime.Now.ToString("HH:mm:ss");  
@@ -57,6 +68,11 @@ namespace _8thMeet
                 MeSound.LoadedBehavior = MediaState.Play;
                 timerAlert.Stop();
             }
+        }
+        //碼表
+        private void timerStopWatch_tick(object sender, EventArgs e)
+        {
+            TxtStopWatch.Text = sw.Elapsed.ToString("hh':'mm':'ss':'fff");
         }
         private void BtnSetAlert_Click(object sender, RoutedEventArgs e)
         {
@@ -77,6 +93,66 @@ namespace _8thMeet
         {
             MeSound.Position = new TimeSpan(0, 0, 1);
             MeSound.LoadedBehavior = MediaState.Play;
+        }
+
+        private void BtnStart_Click(object sender, RoutedEventArgs e)
+        {
+            sw.Start();             
+            timerStopWatch.Start(); 
+        }
+
+
+        private void BtnStop_Click(object sender, RoutedEventArgs e)
+        {
+            sw.Reset();
+            timerStopWatch.Stop();
+            TxtStopWatch.Text = "00:00:00:000";
+            TxtStopWatchLog.Text = "";
+            StopWatchLog.Clear();
+        }
+
+        private void BtnReset_Click(object sender, RoutedEventArgs e)
+        {
+            if (sw.IsRunning)
+            {
+                TxtStopWatchLog.Text = "";
+                if (StopWatchLog.Count == 50)
+                    StopWatchLog.RemoveAt(0);
+                StopWatchLog.Add(TxtStopWatch.Text);
+                int i = 1;
+                while (i <= StopWatchLog.Count)
+                {
+                    TxtStopWatchLog.Text += String.Format("第 {0} 筆紀錄：{1}", i.ToString(), StopWatchLog[StopWatchLog.Count - 1] + "\n");
+                    i++;
+                }
+                sw.Restart();
+            }
+            else
+            {
+                sw.Reset();
+                TxtStopWatch.Text = "00:00:00:000";
+            }
+        }
+
+        private void BtnPause_Click(object sender, RoutedEventArgs e)
+        {
+            sw.Stop();
+            timerStopWatch.Stop();
+        }
+
+        private void BtnLog_Click(object sender, RoutedEventArgs e)
+        {
+            TxtStopWatchLog.Text = "";
+            if (StopWatchLog.Count == 50)
+                StopWatchLog.RemoveAt(0);
+            StopWatchLog.Add(TxtStopWatch.Text);
+
+            int i = 1;
+            while (i <= StopWatchLog.Count)
+            {
+                TxtStopWatchLog.Text += String.Format("第 {0} 筆紀錄：{1}", i.ToString(), StopWatchLog[StopWatchLog.Count - 1] + "\n");
+                i++;
+            }
         }
     }
 }
